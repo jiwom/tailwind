91f33708d3a35a6c74876a64053de4260957cb62
Jerome Sarmiento <jsarmiento@objectbright.com>
2020-12-21 22:32:43 +0800

    Flex box and Alert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    container:{
+    	center: true,
+    	padding: '2rem'
+    }
   },
   variants: {
     extend: {},
